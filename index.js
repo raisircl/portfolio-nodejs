2780bc15cfc2befa3bf471ed60d1704f27329936
raisircl <rai.verma@live.com>
2025-08-04 10:02:00 +0530

    portfolion-common-functions

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const server=createServer((req,res)=>{
     const req_url=req.url;
     const requested_file_path=path.join(__dirname,req_url);
     const file_ext=path.extname(requested_file_path);
-    console.log(`Req File Path: ${requested_file_path}`);
-    console.log(`File Extension: ${file_ext}`);
-    console.log(`req url: ${req.url}`);
+    //console.log(`Req File Path: ${requested_file_path}`);
+    //console.log(`File Extension: ${file_ext}`);
+    // /console.log(`req url: ${req.url}`);
     const content_types={
         '.html':'text/html',
         '.css':'text/css',
@@ -31,12 +31,13 @@ const server=createServer((req,res)=>{
         '/blog-details':'/views/blog-details.html',
     };
     const html_page=html_pages[req_url];
-    console.log(`Html Page ${html_page}`);
+
+    //console.log(`Html Page ${html_page}`);
 
     if(html_page)
     {
             const home_page=path.join(__dirname,`${html_page}`);
-            var page=fs.readFile(home_page,(err,data)=>{
+            fs.readFile(home_page,(err,data)=>{
             if(err)
             {
                 res.setHeader('Content-Type',`${content_type}`);
@@ -45,30 +46,70 @@ const server=createServer((req,res)=>{
             }
             else
             {
-                res.setHeader('Content-Type',`${content_type}`);
-                res.statusCode=200;
-                res.end(data);
+                data=data.toString(); // local scope and global
+               
+                const common_header_page=path.join(__dirname,'/views/common_head.html');
+                //common head read start  
+                fs.readFile(common_header_page,(cerr,common_header)=>{
+                    if(cerr)
+                    {
+                        res.setHeader('Content-Type',`text/html`);
+                        res.statusCode=404;
+                        res.end('<h1>404 common head Not Found</h1>');       
+                    }
+                    else
+                    {
+                        
+                        
+                        data=data.replace('[common-head]',common_header.toString());
+                         
+                    }
+                });
+               // head read end
+                 const common_menu_page=path.join(__dirname,'/views/common-menu.html');
+                //common menu read start  
+                fs.readFile(common_menu_page,(cmerr,common_menu)=>{
+                    if(cmerr)
+                    {
+                        res.setHeader('Content-Type',`text/html`);
+                        res.statusCode=404;
+                        res.end('<h1>404 common menu Not Found</h1>');       
+                    }
+                    else
+                    {
+                        
+                        
+                        data=data.replace('[common-menu]',common_menu.toString());
+                         
+                    }
+                });
+               // head read end
+              const common_footer_page=path.join(__dirname,'/views/common_footer.html');     
+              //common footer read start  
+                fs.readFile(common_footer_page,(cferr,common_footer)=>{
+                    if(cferr)
+                    {
+                        res.setHeader('Content-Type',`text/html`);
+                        res.statusCode=404;
+                        res.end('<h1>404 common footer Not Found</h1>');       
+                    }
+                    else
+                    {
+                        
+                        
+                        data=data.replace('[common-footer]',common_footer.toString());
+                        //console.log(data);  // ok
+                        res.setHeader('Content-Type',`${content_type}`);
+                        res.statusCode=200;
+                    
+                        res.end(data);
+                    }
+                });
+               
             }
         });
     } 
-   else if(req_url.includes('/views'))
-   {
-        var page=fs.readFile(requested_file_path,(err,data)=>{
-            if(err)
-            {
-                res.setHeader('Content-Type',`${content_type}`);
-                res.statusCode=404;
-                res.end('<h1>404 Page Not Found</h1>');
-            }
-            else
-            {
-                res.setHeader('Content-Type',`${content_type}`);
-                res.statusCode=200;
-                res.end(data);
-            }
-        });
-       
-   }
+  
    else if(req_url.includes('assets/css'))
    {
          var page=fs.readFile(requested_file_path,(err,data)=>{
@@ -99,6 +140,7 @@ const server=createServer((req,res)=>{
             {
                 res.setHeader('Content-Type',`${content_type}`);
                 res.statusCode=200;
+               
                 res.end(data);
             }
         });
---
+++ readme.md
@@ -0,0 +1,23 @@
+1. .env File for Secure Email Credentials
+2. npm install dotenv
+3.  EMAIL_USER=your-email@gmail.com
+    EMAIL_PASS=your-app-password
+4. Use: 
+    require('dotenv').config(); // Add this at the top
+    const auth=auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+    
+5. step-by-step usage of querystring module in Node.js (CommonJS style).
+    The querystring module helps you parse and stringify URL query strings, like:
+    ?name=RAI&email=rai@example.com
+    Usage Steps:
+        const url = require('url');
+        const querystring = require('querystring');
+        const parsedUrl = url.parse(req.url);
+        const queryParams = querystring.parse(parsedUrl.query);
+        
+        const name = queryParams.name || 'Guest';
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`Hello, ${name}!`);
